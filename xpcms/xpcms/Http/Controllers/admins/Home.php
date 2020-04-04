dce204697f3d8a8cb9b9c19b48dbd6ef1616c9b3
XiaoqianDing <dxq3442@gmail.com>
2020-04-04 17:26:19 +1100

    add cache

--- xpcms/xpcms/Http/Controllers/admins/Home.php
+++ xpcms/xpcms/Http/Controllers/admins/Home.php
@@ -7,9 +7,26 @@ use xpcms\Http\Controllers\Controller;
 
 class Home extends Controller{
     public function index(Request $req){
-        $data['menus']=$this->get_menus($req->admin);
+        //get data from cache first
+        $hasData = false;
+        if(file_exists($req->admin['username'])){
+            $res = file_get_contents($req->admin['username']);
+            if(!$res==''){
+                $hasData = true;
+            }
+        }
+        if($hasData){
+            $data['menus'] = json_decode($res,true);
+        }else{
+            //request data from database
+            $data['menus']=$this->get_menus($req->admin);
+            //put the content into cache
+            file_put_contents($req->admin['username'],json_encode($data['menus']));
+        }
+
 //        print_r($data['menus']);
 //        exit();
+
         return view('admin.home.index',$data);
     }
 
